00ecc4c81f534424d8f63cc264b2ea34a59a4467
Matthew-uk <esaduviedede@gmail.com>
2024-02-21 01:18:01 +0100

    Removing an Image

--- components/sections/articles/recent.jsx
+++ components/sections/articles/recent.jsx
@@ -2,7 +2,6 @@ import Section from "../../structure/section";
 import Container from "../../structure/container";
 import Image from "next/image";
 
-import Image from "next/image";
 import SectionTitle from "../../blocks/section.title.block";
 
 import Icon from "../../utils/icon.util";
